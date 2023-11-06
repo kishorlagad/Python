ec632c3ee6e2bfd9535cc431d7c13ffbb8523906
kishorlagad <klagad12@gmail.com>
2023-11-06 23:00:42 +0530

    pattern_stars

---
+++ pattern_star.py
@@ -0,0 +1,598 @@
+""" 1
+* 
+* * 
+* * * 
+* * * * 
+* * * * * """
+
+""" for i in range(1,6):
+    for j in range(0,i):
+        print("*",end=" ")
+    print() """
+    
+"""     * 
+      * * 
+    * * * 
+  * * * * 
+* * * * *  """   
+    
+""" for i in range(1,6):
+    for k in range(1,6-i):
+        print(" ",end=" ")
+    for j in range(0,i):
+        print("*",end=" ")
+    print() """
+    
+    
+  
+"""  3
+         *  
+       *   *  
+     *   *   *
+   *   *   *   *
+ *   *   *   *   *   """
+ 
+ 
+        
+""" for i in range(1,6):
+    for k in range(1,6-i):
+        print(" ",end=" ")
+    for j in range(0,i):
+        print(" * ",end=" ")
+    print() """
+    
+"""    4 
+    
+ *   *   *   *   *  
+   *   *   *   *  
+     *   *   *
+       *   *
+         *
+     """
+"""     
+for i in range(5,0,-1):
+    for k in range(1,6-i):
+        print(" ",end=" ")
+    for j in range(0,i):
+        print(" * ",end=" ")
+    print()    """ 
+  
+  
+  
+"""  5
+* * * * * 
+  * * * * 
+    * * * 
+      * * 
+        * 
+"""  
+    
+
+""" for i in range(5,0,-1):
+    for k in range(1,6-i):
+        print(" ",end=" ")
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
+  """
+"""   6
+* * * * * 
+* * * * 
+* * *
+* *
+*
+ """
+ 
+"""    
+for i in range(5,0,-1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print() """
+    
+"""  7
+       *  
+     *   *
+   *   *   *
+ *   *   *   *
+   *   *   *
+     *   *
+       *
+"""
+
+""" 
+for i in range(1,5):
+    for k in range(1,5-i):
+        print(" ",end=" ")
+    for j in range(0,i):
+        print(" * ",end=" ")
+    print() 
+for i in range(3,0,-1):
+    for k in range(1,5-i):
+        print(" ",end=" ")
+    for j in range(0,i):
+        print(" * ",end=" ")
+    print()  """
+   
+   
+"""       Q8
+      1 
+    1 2
+  1 2 3
+1 2 3 4
+""" 
+
+""" 
+for i in range(1,5):
+    for k in range(1,5-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print(j+1,end=" ")
+    print() """
+    
+"""   Q9
+1 
+1 2
+1 2 3
+1 2 3 4
+    """
+""" 
+for i in range(1,5):
+    for j in range(i):
+        print(j+1,end=" ")
+    print() """
+    
+    
+"""Q10
+        1 
+      1   2
+    1   2   3
+  1   2   3   4
+
+"""
+"""     
+for i in range(1,5):
+    for k in range(1,5-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print(" ",j+1,end=" ")
+    print() """
+    
+
+"""     
+for i in range(1,5):
+    for k in range(1,5-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print(" ",j+1,end=" ")
+    print() 
+    
+for i in range(3,0,-1):
+    for k in range(1,5-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print(" ",j+1,end=" ")
+    print()  """
+    
+    
+"""
+        A 
+      A   B
+    A   B   C
+  A   B   C   D
+    """
+    
+""" for i in range(1,5):
+    for k in range(1,5-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print(" ",chr(65+j),end=" ")
+    print()  """
+    
+    
+"""     
+          5 
+        4   4
+      3   3   3
+    2   2   2   2
+  1   1   1   1   1  
+     """
+"""   
+for i in range(5,0,-1):
+    for k in range(1,i):
+        print(" ",end=" ")
+    for j in range(5,i-1,-1):
+        print(" ",i,end=" ")
+    print() """
+
+
+"""
+    1 
+  1 2 1
+1 2 3 2 1
+  1 2 1
+    1
+"""  
+    
+"""     
+for i in range(1,4):
+    for j in range(3,i,-1):
+        print(" ",end=" ")
+    for k in range(1,i+1):
+        print(k,end=" ")
+    for s in range(i-1,0,-1):
+        print(s,end=" ")
+    print()
+    
+for i in range(2,0,-1):
+    for j in range(3,i,-1):
+        print(" ",end=" ")
+    for k in range(1,i+1):
+        print(k,end=" ")
+    for s in range(i-1,0,-1):
+        print(s,end=" ")
+    print() """
+    
+"""    
+for i in range(1,4):
+    for j in range(3,i,-1):
+        print(" ",end=" ")
+    for k in range(1,i+1):
+        print(k,end=" ")
+    for s in range(i-1,0,-1):
+        print(s,end=" ")
+    print()
+    """
+"""     
+        A
+      A B A
+     A B C A
+    A B C D A """
+    
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+# for i in range(1,6):
+#     for j in range(0,i):
+#         print(i,end=" ")
+#     print()
+
+##############################################################
+    
+# 55555
+# 4444
+# 333
+# 22
+# 1
+
+# for i in range(5,0,-1):
+#     for j in range(0,i):
+#         print(i,end="")
+#     print()
+
+############################################################
+
+#     1
+#    22
+#   333
+#  4444
+# 55555
+
+# for i in range(1,6):
+#     for j in range(5,i,-1):
+#         print(" ",end="")
+#     for k in range(0,i):
+#         print(i,end="")
+#     print()
+    
+###############################################################
+
+#     5
+#    44
+#   333
+#  2222
+# 11111   
+
+# for i in range(5,0,-1):
+#     for j in range(1,i):
+#         print(" ",end="")
+#     for k in range(5,i-1,-1):
+#         print(i,end="")  
+#     print() 
+
+################################################################
+
+# 1
+# 12
+# 123
+
+# for i in range(1,4):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+###################################################################
+
+# 1
+# 22
+# 333
+
+# for i in range(1,4):
+#     for j in range(i):
+#         print(i, end="")
+#     print()
+    
+##################################################################
+
+# 1
+# 23
+# 456
+
+# k=1
+# for i in range(1,4):
+#     for j in range(0,i):
+#         print(k, end="")
+#         k+=1
+#     print()
+
+################################################################
+
+#   1
+#  12
+# 123
+
+# for i in range(1,4):
+#     for j in range(4,i,-1):
+#         print(" ",end="")
+#     for k in range(1,i+1):
+#         print(k,end="")
+#     print()
+    
+##############################################################
+
+#   1
+#  12
+# 121
+
+# for i in range(1,4):
+#     for j in range(4,i,-1):
+#         print(" ",end="")
+#     for k in range(1,i+1):
+#         if(k==3):
+#             print(k-2)
+#         else:
+#             print(k,end="")
+#     print()
+    
+##################################################################
+
+# 1
+# 21
+# 321
+
+# for i in range(1,4):
+#     for j in range(i,0,-1):
+#         print(j,end="")
+#     print()
+    
+###################################################################
+
+#   1  
+#  121 
+# 12321
+#  121 
+#   1 
+
+for i in range(1, 4):
+    for j in range(3, i, -1):
+        print(" ", end="")
+    for k in range(1, i + 1):
+        print(k, end="")
+    for s in range(i - 1, 0, -1):
+        print(s, end="")
+    print()
+
+for i in range(2, 0, -1):
+    for j in range(3, i, -1):
+        print(" ", end="")
+    for k in range(1, i + 1):
+        print(k, end="")
+    for s in range(i - 1, 0, -1):
+        print(s, end="")
+    print()
+
+
+    
+#########################################################################################
+
+# a        A
+# bb       BB
+# ccc      CCC
+
+# for i in range(1,4):
+#     for j in range(0,i):
+#         print(chr(96+i),end="")
+#     print()
+    
+#########################################################################################3
+
+# A 
+# AB 
+# ABC
+
+# for i in range(1,4):
+#     for j in range(0,i):
+#         print(chr(65+j),end="")
+#     print()
+
+#####################################################################################
+
+#    A 
+#   AB 
+# ABC
+
+# for i in range(1,4):
+#     for j in range(4,i,-1):
+#         print(" ",end="")
+#     for k in range(0,i):
+#         print(chr(65+k),end="")  
+#     print()
+    
+########################################################################################             
+#    A 
+#   A B 
+#  A B C 
+
+# for i in range(1,4):
+#     for j in range(3,i,-1):
+#         print(" ",end="")
+#     for k in range(0,i):
+#         print(chr(65+k),end=" ")
+#     print()
+    
+#################################################################################################
+
+#         A         
+#       A B C       
+#     A B C D E     
+#   A B C D E F G   
+# A B C D E F G H I 
+#   A B C D E F G   
+#     A B C D E     
+#       A B C       
+#         A  
+
+  
+# for i in range(1,6):
+#     for j in range(5,i,-1):
+#         print(" ",end=" ")
+#     for k in range(0,2*i-1):
+#         print(chr(65+k),end=" ")
+#     print()
+# for i in range(4,0,-1):
+#     for j in range(5,i,-1):
+#         print(" ",end=" ")
+#     for k in range(0,2*i-1):
+#         print(chr(65+k),end=" ")
+#     print()
+    
+#################################################################################################################
+
+# A B C D E F G H I 
+#   A B C D E F G   
+#     A B C D E     
+#       A B C       
+#         A         
+                  
+#         A         
+#       A B C       
+#     A B C D E     
+#   A B C D E F G   
+# A B C D E F G H I 
+# for i in range(6,1,-1):
+#     for j in range(6,i,-1):
+#         print(" ",end=" ")
+#     for k in range(0,2*i-3):
+#         print(chr(65+k),end=" ")
+#     print()
+# for i in range(2,7):
+#     for j in range(6,i,-1):
+#         print(" ",end=" ")
+#     for k in range(0,2*i-3):
+#         print(chr(65+k),end=" ")
+#     print()
+    
+################################################################################################################
+
+# A
+# AB   
+# ABC  
+# ABCD 
+# ABCDE
+# ABCD 
+# ABC  
+# AB   
+# A
+# for i in range(1,6):
+#     for j in range(0,i):
+#         print(chr(65+j),end="")
+#     print()
+# for i in range(4,0,-1):
+#     for j in range(0,i):
+#         print(chr(65+j),end="")
+#     print()
+
+######################################################################################################################
+#    1
+#   121
+#  12321
+ 
+# for i in range(1,4):
+#     for j in range(4,i,-1):
+#         print(" ",end="")
+#     for k in range(1,i+1):
+#         print(k,end="")
+#     for s in range(i-1,0,-1):
+#         print(s,end="")
+#     print()
+    
+########################################################################################################
+
+#    *    
+#   * *   
+#  * * *  
+# * * * * 
+#  * * *  
+#   * *   
+#    *  
+
+# for i in range(0,3):
+#     for j in range(3,i,-1):
+#         print(" ",end="")
+#     for k in range(0,i+1):
+#         print("* ",end="")
+#     print()
+# for i in range(3,-1,-1):
+#     for j in range(3,i,-1):
+#         print(" ",end="")
+#     for k in range(0,i+1):
+#         print("* ",end="")
+#     print()
+    
+#######################################################################################################
+
+#       *     
+#      * *    
+#     *   *   
+#    *     *  
+#   *       * 
+#  *         *
+#   *       * 
+#    *     *  
+#     *   *   
+#      * *    
+#       *
+      
+# n = 6  
+# for i in range(n):
+#     for j in range(n - i):
+#         print(" ", end="")
+#     for k in range(2 * i + 1):
+#         if k == 0 or k == 2 * i:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+# for i in range(n - 2, -1, -1):
+#     for j in range(n - i):
+#         print(" ", end="")
+#     for k in range(2 * i + 1):
+#         if k == 0 or k == 2 * i:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+######################################################################################
